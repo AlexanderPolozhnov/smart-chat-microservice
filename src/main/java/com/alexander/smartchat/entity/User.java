505cb6c8300cee4b0430ac295984327ae061f8ca
Alexander Polozhnov <polozhnov.alex@gmail.com>
2025-07-07 22:18:31 +0300

    refactor(user): add the Email field to User entity

--- src/main/java/com/alexander/smartchat/entity/User.java
+++ src/main/java/com/alexander/smartchat/entity/User.java
@@ -27,6 +27,9 @@ public class User {
     @Column(nullable = false)
     private String passwordHash;
 
+    @Column(nullable = false, unique = true)
+    private String email;
+
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 20)
     private Role role;
