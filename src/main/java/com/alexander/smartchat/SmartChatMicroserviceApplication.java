d5973a72fa9217f2b9c3a5b8ef59f632254dda74
Alexander Polozhnov <polozhnov.alex@gmail.com>
2025-07-07 21:28:48 +0300

    chore: init project with Spring Boot and basic dependencies

---
+++ src/main/java/com/alexander/smartchat/SmartChatMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.alexander.smartchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartChatMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartChatMicroserviceApplication.class, args);
+    }
+
+}
